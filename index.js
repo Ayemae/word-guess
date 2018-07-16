cea2f4f1a39a233db4c9a073afb0fb27dc9c2499
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-07-16 00:59:09 -0400

    .

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ var animals = ["anaconda", "angelfish", "anglerfish", "bluejay", "cardinal", "ca
  "eagle", "fox", "frog", "gecko", "goose", "hawk", "horse", "iguana", "kestrel", "lemur", "lion", "ostrich", "otter", 
  "pelican", "orangutan", "piranha", "pony", "rabbit", "salamander", "seal", "sloth", "tiger", "tortoise", "viper", 
  "walrus", "whale", "wolf", "zebra"]
+
+ var userGuess = "";
+
+ inquirer.prompt(["Choose a letter."]).then(answers => {
+    answers = userGuess;
+});
+
+ document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    if (userGuess > 96 && userGuess < 123) {
+        stuff
+    }else{ console.log("Please press a letter key.")}
+ }
